bbbe7f7d88dfafafc0aca1ad0408caf76fe3cb51
Nam Phương <phuongnam.infl@gmail.com>
2022-06-28 11:18:32 +0700

    fix duplicate messages when choose cur chat-room again

--- cpt-scr/src/pages/Chat/Chat.jsx
+++ cpt-scr/src/pages/Chat/Chat.jsx
@@ -107,6 +107,7 @@ function Chat(props) {
     }
 
     const getPrevMessageBucket = async (chatRoom) => {
+        if (chatRoom[0] == curChatRoom[0]) return
         const seachRes = await axios.get(`${BASE_API_URL}message/previous`, {
             params: {
                 bucketSeq: chatRoom[1].seq,
